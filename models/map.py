f8b38c95e4e9d1f6084cb77d2fea6f1ba0dde8d3
Erfan Abbasi <abbasy.4708@gmail.com>
2025-04-17 21:08:54 +0330

    Add Map class to models with grid initialization

--- models/map.py
+++ models/map.py
@@ -1,1 +1,8 @@
 # map model: represents a map of the world
+
+from typing import List, Dict, Any
+
+class Map:
+    def __init__(self, grid: List[List[int]]):
+        self.grid = grid
+        self.n = len(grid)
