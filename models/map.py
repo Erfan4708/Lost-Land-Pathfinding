c9d89c602132a00b55edb7db347c1cb5f5d3bed5
Erfan Abbasi <abbasy.4708@gmail.com>
2025-04-17 21:20:34 +0330

    feat: add get_cell method to Map class for safe grid access

--- models/map.py
+++ models/map.py
@@ -6,3 +6,10 @@ class Map:
     def __init__(self, grid: List[List[int]]):
         self.grid = grid
         self.n = len(grid)
+    
+    def get_cell(self, x: int, y: int) -> int:
+        if 0 <= x < self.n and 0 <= y < self.n:
+            return self.grid[x][y]
+        else:
+            raise IndexError("Coordinates out of bounds")
+    
