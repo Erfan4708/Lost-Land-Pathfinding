f03dfd48a1226ef6a129039b33912006f08e4270
Erfan Abbasi <abbasy.4708@gmail.com>
2025-04-18 11:12:24 +0330

    feat: add DFSPathFinder class for depth-first search pathfinding

--- algorithms/uninformed.py
+++ algorithms/uninformed.py
@@ -1,1 +1,13 @@
 # uninformed.py: Store uninformed search algorithms
+from models.node import Node
+from models.map import Map
+from typing import List, Dict, Any
+
+class DFSPathFinder:
+    def __init__(self, map_obj: Map):
+        self.map = map_obj
+        self.n = map_obj.n
+        self.visited = set()
+        self.stack = []
+        self.result = None
+        
