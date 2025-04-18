33a4d4e29697297ee649b3f02584e652bc0f82e2
Erfan Abbasi <abbasy.4708@gmail.com>
2025-04-18 11:47:21 +0330

    feat: implement main script to run the app with DFSPathFinder and Map

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 # main.py: Main script to run the app
+
+from models.map import Map
+from algorithms.uninformed import DFSPathFinder
+
+if __name__ == "__main__":
+    test_map = [
+        [-3, -2, -2, "!"],
+        [ 3, -2,  3, -2],
+        [10, -8, "!", 1],
+        [-4, -3, -6, "!"]
+    ]
+
+    map_obj = Map(test_map)
+    dfs_solver = DFSPathFinder(map_obj)
+    result = dfs_solver.solve()
+
+    if result:
+        print("Path found:")
+        print("Path:", result["path"])
+        print("Coins:", result["coins"])
+        print("Stolen:", result["stolen"])
+    else:
+        print("No path found.")
