9b7a84bc3a195b1b3908ad721802a73021e50054
Erfan Abbasi <abbasy.4708@gmail.com>
2025-04-18 11:30:42 +0330

    feat: implement depth-first search algorithm in DFSPathFinder class

--- algorithms/uninformed.py
+++ algorithms/uninformed.py
@@ -10,4 +10,27 @@ class DFSPathFinder:
         self.visited = set()
         self.stack = []
         self.result = None
-        
+
+        def _dfs(self, node) -> Dict:
+            if (node.x, node.y) == (self.n - 1, self.n - 1):
+                self.result = {
+                    "path": node.path,
+                    "coins": node.coins,
+                    "stolen": 0  # No matter if the thief is stolen or not
+                }
+                return True # Finding the goal
+            
+            self.visited.add((node.x, node.y))
+
+            for dx, dy in [(1, 0), (0, 1)]:
+                new_x = node.x + dx
+                new_y = node.y + dy
+
+                if self.map.is_valid(new_x, new_y) and (new_x, new_y) not in self.visited:
+                    next_node = node.copy()
+                    next_node.x = new_x
+                    next_node.y = new_y
+                    next_node.path.append((new_x, new_y))
+
+                    curr_cell = self.map.get_cell(node.x, node.y)
+                    next_cell = self.map.get_cell(new_x, new_y)
