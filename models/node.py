3a64403d7c16746b9cafeb1983393cd31a1d4fca
Erfan Abbasi <abbasy.4708@gmail.com>
2025-04-18 11:47:15 +0330

    fix: update path type annotation in Node class constructor to use built-in list type

--- models/node.py
+++ models/node.py
@@ -2,7 +2,7 @@
 from typing import List, Dict, Any
 
 class Node:
-    def __init__(self, x: int, y: int, path: List[(int, int)], coins: int, has_thief: bool):
+    def __init__(self, x: int, y: int, path: list[(int, int)], coins: int, has_thief: bool):
         self.x = x
         self.y = y
         self.path = path if path else [(x, y)]
